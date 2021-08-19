ca0a50800f5a4ef4c450531898725861624ce296
Morteza-24 <mortezia4444.mz@gmail.com>
2021-08-19 15:25:33 +0430

    minor changes

--- functions.py
+++ functions.py
@@ -1,20 +1,16 @@
-
 # let's write this code in 2 versions: 1. Eng 2. Per
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-list_of_currencies = ['gold', 'dollar', 'etherium', 'bitcoin', 'coin', 'pound', 'euro', 'lire']
+# --------------------- FUNCTIONS -----------------------------------
 
-vertical_list_currencies = ''
-for item in list_of_currencies:
-    vertical_list_currencies += "** " + item.ljust(8) + " **\n"
-
-#print(vertical_list_currencies)
-#--------------------- FUNCTIONS -----------------------------------
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='send the name of a currency to get its price')
 
 def help(update, context):
+    list_of_currencies = ['gold', 'dollar', 'etherium', 'bitcoin', 'coin', 'pound', 'euro', 'lire']
+    vertical_list_currencies = ''
+    for item in list_of_currencies:
+        vertical_list_currencies += "** " + item.ljust(8) + " **\n"
     context.bot.send_message(chat_id=update.effective_chat.id, text='print one of these currencies to get the price\n' + vertical_list_currencies )
 
 def ethereum():
@@ -48,8 +44,5 @@ def euro():
 def lire():
     """ this command gets the price of lire from API and returns it """
     return "api call not implemented yet"
-'''
-def not_supported(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="this type of message is not supported")'''
-#------------------------------------------------------------------------------------
 
+#------------------------------------------------------------------------------------
--- main.py
+++ main.py
@@ -1,34 +1,31 @@
-
 # let's write this code in 2 versions: 1. Eng 2. Per
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import functions, logging
 
-# getting token from file
-token_file = open("token.txt", "r")
-tok = token_file.read()
 
-# main function
 def main(update, context):
-    message = update.message.text
-    if "ethereum" in message.lower():
+    message = update.message.text.lower()
+
+    if "ethereum" in message:
         context.bot.send_message(chat_id=update.effective_chat.id, text=functions.ethereum())
-    elif "bitcoin" in message.lower():
+    elif "bitcoin" in message:
         context.bot.send_message(chat_id=update.effective_chat.id, text=functions.bitcoin())
-    elif "coin" in message.lower():
+    elif "coin" in message:
         context.bot.send_message(chat_id=update.effective_chat.id, text=functions.coin())
-    elif "dollar" in message.lower():
+    elif "dollar" in message:
         context.bot.send_message(chat_id=update.effective_chat.id, text=functions.dollar())
-    elif "euro" in message.lower():
+    elif "euro" in message:
         context.bot.send_message(chat_id=update.effective_chat.id, text=functions.euro())
-    elif "gold" in message.lower():
+    elif "gold" in message:
         context.bot.send_message(chat_id=update.effective_chat.id, text=functions.gold())
-    elif "pound" in message.lower():
+    elif "pound" in message:
         context.bot.send_message(chat_id=update.effective_chat.id, text=functions.pound())
-    elif "lire" in message.lower():
+    elif "lire" in message:
         context.bot.send_message(chat_id=update.effective_chat.id, text=functions.lire())
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text="no currency with this name found")
+
 
 # commands
 start_handler = CommandHandler(['start', 'Start'], functions.start)
@@ -36,17 +33,21 @@ help_handler = CommandHandler(['Help', 'help'], functions.help)
 
 # messages
 main_handler = MessageHandler(Filters.text & ~Filters.command, main)
-#unsupported_message = MessageHandler(~Filters.text & Filters.command, functions.not_supported)
+unsupported_message = MessageHandler((~Filters.text) & (~Filters.command), functions.help)
+
 
 if __name__ == "__main__":
+
+    # getting token from file
+    token_file = open("token.txt", "r")
+    tok = token_file.read()
     updater = Updater(token=tok, use_context=True)
     updater.start_polling()
-    # reporting error
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(main_handler)
-
-
+    
+    # reporting error
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
